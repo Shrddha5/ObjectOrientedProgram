b92a1f7733c1a4910a7f41c5febee8d98f211a7a
Shraddha <bshrddha94@gmail.com>
2026-02-17 16:12:21 +0530

    2 feb

---
+++ src/main/java/superabstraction/super_keyword.java
@@ -0,0 +1,14 @@
+package superabstraction;
+
+public class super_keyword {
+
+}
+
+class Lab184_Private2 {
+    private class a {
+    }
+}
+
+//private class A{
+//
+//}
---
+++ src/main/java/superabstraction/supereg.java
@@ -0,0 +1,56 @@
+package superabstraction;
+
+public class supereg {
+    public static void main(String[] args) {
+        TestCase t1 =  new TestCase();
+
+    }
+}
+
+class BaseClass {
+    private int aa;
+    private String browser;
+
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    BaseClass() {
+        System.out.println("DC- Parent");
+    }
+
+
+    BaseClass(String browser){
+        this.browser = browser;
+        System.out.println("PC- Parent");
+    }
+
+    void openBrowser(String browserName) {
+        System.out.println("Open Browser!! -> " + browserName);
+    }
+
+    void closeBrowser() {
+        System.out.println("Close Browser!!");
+    }
+}
+
+class TestCase extends BaseClass{
+
+    void test(){}
+    String a;
+
+    TestCase()
+    {
+        super("chrome");
+        System.out.println("DC-Child");
+    }
+
+
+
+
+}
